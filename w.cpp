fe887dce6b37f6626fc5be13af892589edf847a8
kkreine <kkreine@my.loyno.edu>
2019-12-09 15:33:21 -0600

    final project

---
+++ w.cpp
@@ -0,0 +1,51 @@
+
+
+/*
+
+Leela -
+You are paired with Amy. 
+The Omicronians are coming!
+
+leela -
+What do you do?
+Hurry up and pick more!
+Run to the ship and fly away!
+Make sure everyone gets in the ship.
+    case: believe bender or not
+
+Fry -
+Run to the ship!
+Look for Bender
+Pick some more
+
+Bender -
+You can't find Fry. What do you do?
+Leave him behind - who cares??
+Wait and search for him
+    You wouldn't do that. You're Bender. You go back to the ship
+
+Leela asks where Fry is - you were paired with him
+    I think we left him behind
+    Oh he's just in the ship bathroom!
+
+You return to Earth. People keep asking where Fry is
+    We left him!
+    Oh he's off doing a delivery! He's been busy at work.
+
+After a week, no one's seen Fry. He gets an award for all of his hard work you've been
+reporting on. What do you do?
+    Tell them you left him behind.
+    Go rescue him!
+
+    
+    
+
+
+
+
+
+
+
+
+
+*/
